cd1044a30973719fc9d40bcff82104b5f5473f95
Paulo Santos <paulorfsantos17@gmail.com>
2023-08-08 15:34:37 -0300

    first commit - config initial app.ts and connect BD mongoose

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { Application } from 'express'
+import { connect } from './infra/database'
+
+class App {
+  public app: Application
+
+  constructor() {
+    this.app = express()
+    this.middlewaresInicialize()
+    this.inicializeRoutes()
+    this.interceptionError()
+    connect()
+  }
+
+  interceptionError() {
+    // this.app.use()
+  }
+
+  inicializeRoutes() {
+    // this.app.use('/')
+  }
+  
+  middlewaresInicialize() {
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({extended:true}))
+  }
+
+  listen() {
+    this.app.listen(3333, () => console.log('Server is running!'))
+  }
+}
+
+export {App}
